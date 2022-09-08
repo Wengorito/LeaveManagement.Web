9a1b96b0068396496be78fa18af131f59cf9f10f
Grzegorz Kowalski <hvitr.ulfr@gmail.com>
2022-09-08 21:29:26 +0200

    Add employee and data tables to DB.

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public string NumberOfDays { get; set; }
+
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public string EmployeeId { get; set; }
+    }
+}
