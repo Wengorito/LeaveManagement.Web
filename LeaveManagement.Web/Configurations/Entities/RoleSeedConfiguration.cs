6053cf342df8cf92cad55981dd3217759a33895e
Grzegorz Kowalski <hvitr.ulfr@gmail.com>
2022-09-11 17:23:53 +0200

    Scaffolded pages for registration and login. Added default users and roles.

---
+++ LeaveManagement.Web/Configurations/Entities/RoleSeedConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LeaveManagement.Web.Configurations.Entities
+{
+    public class RoleSeedConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole
+                {
+                    Id = "bc181ad9-73d4-4114-8277-1059f8a8aaf8",
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR"
+                },
+                new IdentityRole
+                {
+                    Id = "bc654ba9-71d4-4114-8123-1000a1c8aaf8",
+                    Name = "User",
+                    NormalizedName = "USER"
+                });
+        }
+    }
+}
---
+++ LeaveManagement.Web/Configurations/Entities/UserRoleSeedConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LeaveManagement.Web.Configurations.Entities
+{
+    public class UserRoleSeedConfiguration : IEntityTypeConfiguration<IdentityUserRole<string>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+        {
+            builder.HasData(
+                new IdentityUserRole<string>
+                {
+                    UserId = "bc611ca9-706a-3214-8003-1021b1c8aef8",
+                    RoleId = "bc181ad9-73d4-4114-8277-1059f8a8aaf8"
+                },
+                new IdentityUserRole<string>
+                {
+                    UserId = "bc61a1a9-136a-3214-1103-1021b1c8abcd",
+                    RoleId = "bc654ba9-71d4-4114-8123-1000a1c8aaf8"
+                });
+        }
+    }
+}
