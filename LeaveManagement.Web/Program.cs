bf20773d60e76a225ea71f19370b6b3c27dad4a8
Grzegorz Kowalski <grzegorz.kowalski@pcosa.com.pl>
2022-11-02 15:18:34 +0100

    UNDO previous commit + test message

--- LeaveManagement.Web/Program.cs
+++ LeaveManagement.Web/Program.cs
@@ -41,17 +41,16 @@ var app = builder.Build();
 app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseMigrationsEndPoint();
-//}
-//else
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-app.UseMigrationsEndPoint();
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
